3099f8b5da08548988f185c7592b4345784a9270
Heron Yang <v6510young88@gmail.com>
2014-01-29 14:17:25 +0000

    change DEBUG MODE to False

--- plate_server/settings.py
+++ plate_server/settings.py
@@ -8,7 +8,7 @@ BROKER_URL = 'amqp://guest:guest@localhost//'
 
 PROJECT_DIR = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -30,7 +30,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.plate.tw']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
