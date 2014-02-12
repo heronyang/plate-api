b3fcc9ede2a963cc8f727a2597b0172125220c7c
Heron Yang <v6510young88@gmail.com>
2014-02-12 20:12:57 +0800

    sms resend delay time changes to 3 mins

--- api/const.py
+++ api/const.py
@@ -5,7 +5,7 @@ class Configs(object):
     MAX_TOTAL_PRICE_PER_ORDER = 3000
     MAX_ACCEPTABLE_FAILURE = 2
     PRIOR_NUMBER_SLIPS = 3
-    MINUTES_LOCK_BETWEEN_REGISTRATIONS = 15
+    MINUTES_LOCK_BETWEEN_REGISTRATIONS = 3
 
     # Normally only allow one oustanding order per customer
     # Turn on only for testing
