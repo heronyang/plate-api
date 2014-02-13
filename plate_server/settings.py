c0a9d03972fd83d8e9ddcee892895116f89ac139
Heron Yang <v6510young88@gmail.com>
2014-02-13 08:46:50 +0000

    fix bug: flower kills the celery workers

--- plate_server/settings.py
+++ plate_server/settings.py
@@ -173,7 +173,7 @@ AUTHENTICATION_BACKENDS = (
 
 BROKER_URL = 'amqp://guest:guest@localhost//'
 CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend'
-CELERY_ACCEPT_CONTENT = [ 'pickle' ]
+CELERY_ACCEPT_CONTENT = [ 'json', 'pickle' ]
 CELERY_TASK_SERIALIZER = 'pickle'
 # enable 'pool_restart' command for reloading tasks
 CELERYD_POOL_RESTARTS = True
